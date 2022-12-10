74c51bdf81c34abeffcaadb9e9e1ebca560dac59
cultstatue <saltedchurros@gmail.com>
2022-12-09 21:09:50 -0500

    basic mobile friendly layout established

---
+++ src/pages/Home/index.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import {
+    Container,
+    Row,
+    Col
+} from 'react-bootstrap'
+import "./index.css";
+
+function Home() {
+    return (
+        <Container>
+          <Row>
+            <Col lg={2} xs={1}></Col>
+            <Col lg={8} xs={10}>
+                <div className="home-container">
+                home page here
+                </div>
+            </Col>
+            <Col lg={2} xs={1}></Col>
+          </Row>
+        </Container>
+    );
+}
+
+export default Home
