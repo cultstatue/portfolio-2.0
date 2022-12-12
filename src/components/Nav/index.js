6787d2af8d324e40f7958fb297ec45dff5bd4c37
cultstatue <saltedchurros@gmail.com>
2022-12-12 14:46:27 -0500

    added some colors font and padding

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -8,9 +8,28 @@ import "./index.css";
 
 function Nav() {
     return(
-        <div>
-            navbar
-        </div>
+
+        <Container>
+            <Row className="navbar">
+                <Col className="d-flex justify-content-center">
+                    about
+                </Col>
+
+                <Col className="d-flex justify-content-center">
+                    resume
+                </Col>
+
+                <Col className="d-flex justify-content-center">
+                    work
+                </Col>
+
+                <Col className="d-flex justify-content-center">
+                    contact
+                </Col>
+            </Row>
+            
+        </Container>
+
     );
 }
 
