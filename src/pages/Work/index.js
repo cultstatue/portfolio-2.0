8478e907874cc30bcb21376f2bda62611136fdf9
cultstatue <saltedchurros@gmail.com>
2022-12-13 16:29:41 -0500

    added some styles to wotk section

--- src/pages/Work/index.js
+++ src/pages/Work/index.js
@@ -3,21 +3,36 @@ import {
     Container,
     Row,
     Col,
-    Image
+    Image,
+    Button
 } from 'react-bootstrap'
+import "./index.css"
 
 function Work() {
     return (
         <section>
             <Container>
-                <Row>
-                    <Col>
-                    box one
+                <Row className="work-element">
+                    <Col xxl={6} sm={12}>
+                        <Image
+                        src={require(`../../assets/test-header.png`)}
+                        fluid
+                        className="work-img"
+                        ></Image>
                     </Col>
 
-                    <Col>
-                    Box Two
-                    </Col>
+                    <Col xxl={6} sm={12}>
+                        <h2>Work Title</h2>
+                        <h3>subtitle(madewith will go here)</h3>
+                        <div className="work-content">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis sem id metus tempor dictum. Curabitur ut diam a ligula pretium ultricies sit amet ut enim. Nam accumsan magna ante, non imperdiet odio viverra non.
+                        </div>
+                        <div className="d-grid">
+                            <Button className="git-button">
+                                Github button
+                            </Button>
+                        </div>
+                    </Col>  
                 </Row>
             </Container>
         </section>
