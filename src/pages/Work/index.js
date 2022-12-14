f5d481b2ba38867231d696ddcbd1e52dba87d490
cultstatue <saltedchurros@gmail.com>
2022-12-13 19:04:55 -0500

    fixed up more styles, slowly adding projects

--- src/pages/Work/index.js
+++ src/pages/Work/index.js
@@ -4,14 +4,76 @@ import {
     Row,
     Col,
     Image,
-    Button
+    Button,
 } from 'react-bootstrap'
 import "./index.css"
 
 function Work() {
+
+    const work = [
+        {
+            name: "Petyard",
+            img: "petyard.png",
+            subtitle: "MERN stack, React-Bootstrap",
+            desc: "lorem ipsumgdfdgdf gfdsgfds ",
+            github: "https://github.com/cultstatue/petyard"
+
+        },
+        {
+            name: "Bragging Rights",
+            img: "bragging-rights.png",
+            subtitle: "mySQL, Node.js, Express, Bootstrap",
+            desc: "lorem ipsumgdfdgdf gfdsgfds ",
+            github: "https://github.com/cultstatue/bragging-rights"
+
+        },
+        {
+            name: "Ready?",
+            img: "ready.png",
+            subtitle: "Javascript, server-side APIs, Bulma",
+            desc: "lorem ipsumgdfdgdf gfdsgfds ",
+            github: "https://github.com/cultstatue/ready-s"
+
+        }
+    ]
+
     return (
         <section>
             <Container>
+                {work.map((project) => (
+                    <Row className="work-element">
+                        <Col xxl={6} sm={12}>
+                            <Image
+                            src={require(`../../assets/screenshots/${project.img}`)}
+                            fluid
+                            className="work-img"
+                            alt={project.name}
+                            key={project.name}
+                            ></Image>
+                        </Col>
+
+                        <Col xxl={6} sm={12}>
+                        <h2>{project.name}</h2>
+                        <h3>{project.subtitle}</h3>
+                        <div className="work-content">
+                            {project.desc}
+                        </div>
+                        <div className="d-grid">
+                            
+                            <a href={project.github}>
+                            <div className="d-grid">
+                                <Button variant="git-button">
+                                    Visit this project on Github
+                                </Button>
+                            </div>
+                            </a>
+                        
+                            
+                        </div>
+                        </Col>
+                    </Row>  
+                ))}
+
                 <Row className="work-element">
                     <Col xxl={6} sm={12}>
                         <Image
